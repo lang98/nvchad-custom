9377a20dc2faaa814d7f9db9a34b4c32503f99dd
Ryan Qin <qinang5543@gmail.com>
2022-12-26 04:42:47 -0500

    minor

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,11 +1,10 @@
 return {
-["neovim/nvim-lspconfig"] = {
-  config = function()
+  ["neovim/nvim-lspconfig"] = {
+    config = function()
       require "plugins.configs.lspconfig"
       require "custom.plugins.lspconfig"
     end,
   },
   ["kdheepak/lazygit.nvim"] = {},
-  ['folke/which-key.nvim'] = { disable = false  },
+  ['folke/which-key.nvim'] = { disable = false },
 }
-  
